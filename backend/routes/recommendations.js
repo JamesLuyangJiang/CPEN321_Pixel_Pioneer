3f14af67d2d8385372cd3f0026217dec3fa39a2f
Jiayu Huang <harthuang990517@gmail.com>
2023-10-23 15:14:24 -0700

    add stub get and post response for recommmendations route

--- backend/routes/recommendations.js
+++ backend/routes/recommendations.js
@@ -1,2 +1,10 @@
 const express = require('express')
 const router = express.Router()
+
+router.get('/recommendations', (req, res) => {
+    res.send("This get request should return a list of recommended places for users without any filteration logic.")
+})
+
+router.post('recommendations', (req, res) => {
+    res.send("This post request should return a list of recommended places based on the filters provided by the users.")
+})
