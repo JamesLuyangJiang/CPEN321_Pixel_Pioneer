55bb194231283d30d4f007c11f4451468be3af99
whh1123 <wh_nwafu@163.com>
2023-10-28 23:28:05 -0700

    migrate prev recommendations to apimanager, add recommendations and users modules

--- backend/routes/recommendations.js
+++ backend/routes/recommendations.js
@@ -1,64 +1,24 @@
 const express = require("express");
 const axios = require("axios");
 const router = express.Router();
+const apiManager = require("./apimanager");
 
 const CONFIDENTIAL_WEATHER_API_KEY = "29af4c07ebdd4189a0b222326232410";
 
-router.get("/", async (req, res) => {
-  const client_ip = "184.68.183.186"; // hardcoded for testing purpose, TODO: need to replace with req.ip in the future
-  try {
-    const response = await axios.get(`https://ipinfo.io/${client_ip}/json`); // api to get geolocation of the client based on their ip, TODO: move to api manager later
-    const geolocationData = response.data;
-    const lat = geolocationData.loc.split(",")[0];
-    const lon = geolocationData.loc.split(",")[1];
+router.get("/:radius/:maximum_city_number", async (req, res) => {
+    const radius = req.params.radius == null ? 1000 : req.params.radius;
+    const maximum_city_number = req.params.maximum_city_number == null ? 10 : req.params.maximum_city_number;
+
     try {
-      const list_of_cities_response = await axios.get(
-        `https://geogratis.gc.ca/services/geoname/en/geonames.json?lat=${lat}&lon=${lon}&radius=100`
-      ); // api to get a list of places around the geolocation of the client, TODO: need to move to apimanager in the future
-      const actual_data_items = list_of_cities_response.data.items;
-      var customized_response_list = []; // the actual json response of this request
+        var nearby_observatory_list = [];
+        nearby_observatory_list = await apiManager.fetchNearbyObservatoryFromAPIs(radius);
 
-      for (let i = 0; i < actual_data_items.length; i++) {
-        const currentObject = actual_data_items[i];
-        var customized_response = {
-          name: currentObject.name,
-          latitude: currentObject.latitude,
-          longitude: currentObject.longitude,
-          distance: calculateDistance(
-            lat,
-            lon,
-            currentObject.latitude,
-            currentObject.longitude
-          ),
-        };
-        customized_response_list.push(customized_response);
-      }
-      sortByDistance(customized_response_list);
-      customized_response_list = getFirstTwenty(customized_response_list); // only getting the closest 20 places since the next API call will take a long time
-
-      const weatherAPIRequests = customized_response_list.map(async (item) => {
-        try {
-          const weather_forecast_response = await axios.get(
-            `http://api.weatherapi.com/v1/forecast.json?key=${CONFIDENTIAL_WEATHER_API_KEY}&q=${item.latitude},${item.longitude}&days=10&aqi=no&alerts=no`
-          );
-          const weather_data =
-            weather_forecast_response.data.forecast.forecastday;
-          item.weatherForecast = filterWeatherForecast(weather_data);
-        } catch (err) {
-          item.weatherForecast = [];
-          console.error("Call weather API failed...");
-        }
-      });
-
-      await Promise.all(weatherAPIRequests);
-
-      res.json(customized_response_list);
+        var recommendation_list = generateRecommendationList(nearby_observatory_list, radius, maximum_city_number);
+        // Process the data or send it as a response
+        res.json(recommendation_list);
     } catch (error) {
-      res.status(500).json({ error: "Unable to retrieve geolocation data" });
+        res.status(500).json({ error: 'Failed to fetch data from the API' });
     }
-  } catch (error) {
-    res.status(500).json({ error: "Unable to retrieve geolocation data" });
-  }
 });
 
 router.post("/", (req, res) => {
@@ -66,82 +26,58 @@ router.post("/", (req, res) => {
     "This post request should return a list of recommended places based on the filters provided by the users."
   );
 });
+function generateRecommendationList(nearby_observatory_list, radius, maximum_city_number) {
 
-// Acknowledgement: produced by ChatGPT
-function calculateDistance(lat1, lon1, lat2, lon2) {
-  const earthRadius = 6371; // Radius of the Earth in kilometers
+    // Filtering results with condition_score of 3 and below
+    // Filter the weatherForecast for each item in the list
+    nearby_observatory_list.forEach(item => {
+       item.weatherForecast = item.weatherForecast.filter(forecast => forecast.condition_score > 3);
+       item.weatherForecast.sort((a, b) => {
+           return  b.condition_score - a.condition_score;
+       });
+    });
+    const maxDistance = radius;
+    const minDistance = 0.01;
+    // Sort the weatherForecast array for each observatory based on condition_score
+    nearby_observatory_list.forEach(item => {
+        if(item.distance > maxDistance) {
+            maxDistance = item.distance;
+        } else if (item.distance < minDistance) {
+            minDistance = item.distance;
+        }
+    });
+    nearby_observatory_list.forEach(item => {
+        const distanceScore = (1 - (item.distance - minDistance) / (maxDistance - minDistance)) * 0.4;
+        const conditionScore = item.weatherForecast[0].condition_score * 0.6 / 9;
+        item.totalScore = (distanceScore + conditionScore) * 100;
+    });
+    if (nearby_observatory_list.length < maximum_city_number) {
+        maximum_city_number = nearby_observatory_list.length;
+    }
 
-  // Convert latitude and longitude from degrees to radians
-  const lat1Rad = (Math.PI * lat1) / 180;
-  const lon1Rad = (Math.PI * lon1) / 180;
-  const lat2Rad = (Math.PI * lat2) / 180;
-  const lon2Rad = (Math.PI * lon2) / 180;
+    var recommendation_list = []; // the actual json response of this request
 
-  // Haversine formula
-  const dLat = lat2Rad - lat1Rad;
-  const dLon = lon2Rad - lon1Rad;
-
-  const a =
-    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-    Math.cos(lat1Rad) *
-      Math.cos(lat2Rad) *
-      Math.sin(dLon / 2) *
-      Math.sin(dLon / 2);
-
-  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-
-  // Calculate the distance in kilometers
-  const distance = earthRadius * c;
-
-  return distance;
+    for (let i = 0; i < maximum_city_number; i++) {
+      const currentObservatory = nearby_observatory_list[i];
+      var recommended_observatory = {
+          name: currentObservatory.name,
+          distance: currentObservatory.distance.toFixed(2),
+          date: currentObservatory.weatherForecast[0].date,
+          condition: currentObservatory.condition,
+          totalScore: currentObservatory.totalScore.toFixed(2),
+      };
+      recommendation_list.push(recommended_observatory);
+    }
+    return recommendation_list;
 }
-
-// Acknowledgement: produced by ChatGPT
-function sortByDistance(objects) {
-  // Use the sort() method to sort the list by the "distance" field
-  objects.sort((a, b) => a.distance - b.distance);
-}
-
-// Only using 00:00, 06:00, 12:00, and 18:00 weather conditions of the day
-function filterWeatherForecast(weatherForecast) {
-  const filteredForecast = [];
-
-  for (const forecast of weatherForecast) {
-    const date = forecast.date;
-    var conditions = [];
-
-    conditions.push({
-      time: forecast.hour[0].time,
-      condition: forecast.hour[0].condition.text,
-      //   airquality: forecast.hour[0].air_quality,
-    });
-    conditions.push({
-      time: forecast.hour[6].time,
-      condition: forecast.hour[6].condition.text,
-      //   airquality: forecast.hour[6].air_quality,
-    });
-    conditions.push({
-      time: forecast.hour[12].time,
-      condition: forecast.hour[12].condition.text,
-      //   airquality: forecast.hour[12].air_quality,
-    });
-    conditions.push({
-      time: forecast.hour[18].time,
-      condition: forecast.hour[18].condition.text,
-      //   airquality: forecast.hour[18].air_quality,
-    });
-
-    filteredForecast.push({ date: date, conditions: conditions });
+function getTopNObjects(list, n) {
+  if (n < 0) {
+    n = 0;
   }
-
-  return filteredForecast;
-}
-
-function getFirstTwenty(list) {
-  if (list.length <= 20) {
+  if (list.length <= n) {
     return list;
   } else {
-    return list.slice(0, 20);
+    return list.slice(0, n);
   }
 }
 
