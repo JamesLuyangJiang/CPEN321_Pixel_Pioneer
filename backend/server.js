f1f2e34aa820df90b2cda4af14f8ffd5af8b53c9
Jiayu Huang <harthuang990517@gmail.com>
2023-10-29 14:46:13 -0700

    change port from 3000 to 8081

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 8081;
 
 // const { MongoClient } = require("mongodb");
 // const uri = "mongodb://localhost:27017";
