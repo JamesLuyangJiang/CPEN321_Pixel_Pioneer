c1360844addead4620d5ddfb5be558ca8b3128e4
whh1123 <58422140+whh1123@users.noreply.github.com>
2023-11-01 15:35:05 -0700

    Update server.js

--- testBnd/server.js
+++ testBnd/server.js
@@ -1,14 +1,9 @@
 const https = require("https");
 const fs = require("fs");
 
-const fcm = require("./routes/fcm");
-
 const express = require("express");
 const app = express();
 const port = 8081;
-
-const testAppToken =
-  "cBc-1l_iQvuKJPqKSMSdJO:APA91bEqjbbmKNQ6MzV69pXccqedzrfGlHa_H18KdgasEiulRHILZ9yIIetoGEOwwnjlak-h8dHsrApXOWuS1NkCBxxbnytbZghniPv21oZvRjiv4FhicscBvsXcq93961r9j-8LuHVB";
 
 // Firebase
 var admin = require("firebase-admin");
@@ -22,7 +17,7 @@ const uri = "mongodb://localhost:27017";
 const client = new MongoClient(uri);
 
 const recommendationsRoutes = require("./routes/recommendations");
-const schedulingRoutes = require("./routes/events");
+const schedulingRoutes = require("./routes/scheduling");
 const userRoutes = require("./routes/users");
 
 app.use(express.json());
@@ -41,42 +36,6 @@ async function run() {
   } catch (err) {
     console.log(err);
     await client.close();
-  }
-}
-
-async function retrieveGlobalUserIDCounter() {
-  try {
-    var global_user_id_counter = await client
-      .db("astronomy")
-      .collection("globaluseridcounter")
-      .find()
-      .toArray();
-    console.log(typeof global_user_id_counter);
-    if (Object.keys(global_user_id_counter).length === 0) {
-      console.log("new server");
-      await client
-        .db("astronomy")
-        .collection("globaluseridcounter")
-        .insertOne({ count: 1 });
-      console.log("started new count at 1...");
-    } else {
-      console.log("already started counter...");
-      const user_id_obj = await client
-        .db("astronomy")
-        .collection("globaluseridcounter")
-        .find()
-        .toArray();
-      const user_id_final = user_id_obj[0].count;
-      console.log(user_id_final);
-
-      // logic for update the next user_id
-      // await client
-      //   .db("astronomy")
-      //   .collection("globaluseridcounter")
-      //   .replaceOne({ count: user_id_final }, { count: user_id_final + 1 });
-    }
-  } catch (err) {
-    console.log(err);
   }
 }
 
@@ -102,23 +61,20 @@ async function sendNotification(registrationToken) {
     });
 }
 
-// app.listen(port, async () => {
-//   console.log(`HTTPS server listening on port ${port}`);
-//   run();
-//   retrieveGlobalUserIDCounter();
-//   sendNotification(testAppToken);
-// });
+//app.listen(port, async () => {
+//  console.log(`HTTPS server listening on port ${port}`);
+//  run();
+//});
 
-https
-  .createServer(
-    {
-      key: fs.readFileSync("privkey.pem"),
-      cert: fs.readFileSync("cert.pem"),
-    },
-    app
-  )
-  .listen(port, async () => {
-    console.log(`HTTPS server listening on port ${port}`);
-    run();
-    retrieveGlobalUserIDCounter();
-  });
+ https
+   .createServer(
+     {
+       key: fs.readFileSync("privkey.pem"),
+       cert: fs.readFileSync("cert.pem"),
+     },
+     app
+   )
+   .listen(port, async () => {
+     console.log(`HTTPS server listening on port ${port}`);
+     run();
+   });
