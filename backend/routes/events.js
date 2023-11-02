4ca12e9a5da528b3c0dc65d46dd960dce6dae260
Jiayu Huang <harthuang990517@gmail.com>
2023-11-02 14:42:46 -0700

    add DELETE handler for events.js and refactor events.js

--- backend/routes/events.js
+++ backend/routes/events.js
@@ -7,6 +7,18 @@ const { MongoClient } = require("mongodb");
 const uri = "mongodb://localhost:27017";
 const client = new MongoClient(uri);
 
+async function manageDB() {
+  try {
+    await client.connect();
+    console.log("Successfully connected to the database");
+    return true;
+  } catch (err) {
+    console.log(err);
+    await client.close();
+    return false;
+  }
+}
+
 // TODO: change the file name to scheduling.js
 router.get("/:id/events", async (req, res) => {
   try {
@@ -17,7 +29,7 @@ router.get("/:id/events", async (req, res) => {
       const responseObj = await client
         .db("astronomy")
         .collection("events")
-        .find({ id: Number(id) })
+        .find({ userid: id })
         .toArray();
       console.log(responseObj);
       res.status(200).send(responseObj);
@@ -37,19 +49,73 @@ router.post("/:id/events", async (req, res) => {
     try {
       const { id } = req.params;
       console.log(id);
-      req.body.id = Number(id);
-      console.log(req.body);
-      await client.db("astronomy").collection("events").insertOne(req.body);
-      if (req.body.notificationToken && req.body.date) {
-        await notification.sendNotification(req.body.notificationToken, req.body.date);
+      const checkIDExists = await client
+        .db("astronomy")
+        .collection("users")
+        .findOne({ userid: id });
+      if (!checkIDExists) {
+        res.status(400).send("FAILED because ID does not exist in database.");
+      } else {
+        event_obj = {
+          userid: id,
+          name: req.body.name,
+          date: req.body.date,
+          notificationToken: req.body.notificationToken,
+        };
+        await client.db("astronomy").collection("events").insertOne(event_obj);
+        if (req.body.notificationToken && req.body.date) {
+          await notification.sendNotification(
+            req.body.notificationToken,
+            req.body.date
+          );
+        }
+        res.status(200).send("Event added successfully\n");
       }
-      res.status(200).send("Event added successfully\n");
     } catch (err) {
       res.status(400).send(err);
     }
   } catch (err) {
     console.log(err);
     await client.close();
+  }
+});
+
+router.delete("/:id/events/delete", async (req, res) => {
+  const isConnected = manageDB();
+  if (isConnected) {
+    try {
+      const { id } = req.params;
+      console.log(id);
+      const checkIDExists = await client
+        .db("astronomy")
+        .collection("users")
+        .findOne({ userid: id });
+      console.log(checkIDExists);
+      if (!checkIDExists) {
+        res.status(400).send("FAILED because ID does not exist in database.");
+      } else {
+        const checkEventExists = await client
+          .db("astronomy")
+          .collection("events")
+          .findOne({ userid: id, name: req.body.name });
+        console.log(checkEventExists);
+        if (!checkEventExists) {
+          res
+            .status(400)
+            .send(
+              "FAILED because this ID does not have this event in database."
+            );
+        } else {
+          await client
+            .db("astronomy")
+            .collection("events")
+            .findOneAndDelete({ userid: id, name: req.body.name });
+          res.status(200).send("Event deleted successfully\n");
+        }
+      }
+    } catch (err) {
+      res.status(400).send(err);
+    }
   }
 });
 
