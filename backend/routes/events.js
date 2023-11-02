91ce5deaab497d4b7dd92006d6983235b240f68c
whh1123 <58422140+whh1123@users.noreply.github.com>
2023-11-01 23:41:38 -0700

    Update events.js

--- backend/routes/events.js
+++ backend/routes/events.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const apiManager = require("./apimanager");
+const notification = require("./notification");
 const router = express.Router();
 
 const { MongoClient } = require("mongodb");
 const uri = "mongodb://localhost:27017";
 const client = new MongoClient(uri);
 
+// TODO: change the file name to scheduling.js
 router.get("/:id/events", async (req, res) => {
   try {
     await client.connect();
@@ -37,6 +40,9 @@ router.post("/:id/events", async (req, res) => {
       req.body.id = Number(id);
       console.log(req.body);
       await client.db("astronomy").collection("events").insertOne(req.body);
+      if (req.body.notificationToken && req.body.date) {
+        await notification.sendNotification(req.body.notificationToken, req.body.date);
+      }
       res.status(200).send("Event added successfully\n");
     } catch (err) {
       res.status(400).send(err);
