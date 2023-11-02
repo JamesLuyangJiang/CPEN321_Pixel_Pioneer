83f6e88e3e669fda76fc600d906c457e073d718f
Jiayu Huang <harthuang990517@gmail.com>
2023-11-02 11:16:39 -0700

    refactor GET request handler in routes/users.js

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -21,9 +21,7 @@ async function createProfile(req, res) {
   const isConnected = await manageDB();
   if (isConnected) {
     try {
-      console.log(req.body);
       const user_id = uuid.v4();
-      console.log(user_id);
       responseObj = {
         userid: user_id,
         message: "Successfully created user profile",
@@ -43,66 +41,50 @@ async function createProfile(req, res) {
           distance: req.body.distance,
           maxnumberofobservatories: req.body.maxnumberofobservatories,
         });
-        console.log(responseObj);
         res.send(responseObj);
       }
     } catch (err) {
-      console.log(err);
       res.status(400).send(err);
     }
   }
 }
 
 async function getProfile(req, res) {
+  const { userid } = req.params;
   try {
-    await client.connect();
-    console.log("Successfully connected to the database");
-    const { userid } = req.params;
-    try {
-      // Fetch the current user by userid
-      const user = await client
-        .db("astronomy")
-        .collection("users")
-        .findOne({ userid: Number(userid) });
+    // Fetch the current user by userid
+    const user = await client
+      .db("astronomy")
+      .collection("users")
+      .findOne({ userid: userid });
 
-      if (!user) {
-        res.status(404).send("User not found");
-      }
-
-      res.status(200).send("The specified user id is: " + user.userid);
-    } catch (err) {
-      res.status(400).send(err);
+    if (!user) {
+      res.status(404).send("User not found");
+    } else {
+      res.status(200).send(user);
     }
   } catch (err) {
-    console.log(err);
-    await client.close();
+    res.status(400).send(err);
   }
 }
 
 async function updateProfile(req, res) {
   try {
-    await client.connect();
-    console.log("Successfully connected to the database");
-    try {
-      const { userid, username, email, distance, maxnumberofobservatories } =
-        req.params;
-      console.log(userid, username, email, distance, maxnumberofobservatories);
+    const { userid, username, email, distance, maxnumberofobservatories } =
+      req.params;
+    console.log(userid, username, email, distance, maxnumberofobservatories);
 
-      await client.db("astronomy").collection("users").insertOne(req.body);
+    await client.db("astronomy").collection("users").insertOne(req.body);
 
-      res.status(200).send("User added successfully\n");
-    } catch (err) {
-      res.status(400).send(err);
-    }
+    res.status(200).send("User added successfully\n");
   } catch (err) {
-    console.log(err);
-    await client.close();
+    res.status(400).send(err);
   }
 }
 
 router.get("/get/:userid", getProfile);
-router.post("/create", createProfile);
 router.put("/put/:userid", updateProfile);
+router.post("/create", createProfile);
 
 // router.post(
 //   "/update/:userid/:username/:email/:distance/:maxnumberofobservatories",
