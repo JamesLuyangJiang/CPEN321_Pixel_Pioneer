72e3700d7843a8ecfd4ae181bd54e620e7dd1bb3
whh1123 <58422140+whh1123@users.noreply.github.com>
2023-11-01 23:36:17 -0700

    Update users.js

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,63 +1,73 @@
 const express = require("express");
 const router = express.Router();
-
+const uuid = require('uuid');
 const { MongoClient } = require("mongodb");
 const uri = "mongodb://localhost:27017";
 const client = new MongoClient(uri);
 
 router.get("/:userid", async (req, res) => {
   try {
-          await client.connect();
-          console.log("Successfully connected to the database inside users module");
-          const { userid } = req.params;
-          try {
-            // Fetch the current user by userid
-            const user = await client
-                .db("astronomy")
-                .collection("users")
-                .findOne({ userid: userid });
-            if (!user) {
-                res.status(404).send("User not found");
-            }
+    await client.connect();
+    console.log("Successfully connected to the database");
+    const { userid } = req.params;
+    try {
+      // Fetch the current user by userid
+      const user = await client
+        .db("astronomy")
+        .collection("users")
+        .findOne({ userid: Number(userid) });
 
-            res.status(200).send("The specified user id is: " + user.userid);
-          }
-          catch (err) {
-            res.status(400).send(err);
-          }
+      if (!user) {
+        res.status(404).send("User not found");
+      }
+
+      res.status(200).send("The specified user id is: " + user.userid);
+    } catch (err) {
+      res.status(400).send(err);
+    }
   } catch (err) {
     console.log(err);
     await client.close();
-    }
+  }
 });
 
-// router.post("/create", async (req, res) => {
-//   try {
-//     await client.connect();
-//     console.log("Successfully connected to the database");
-//     try {
-//       console.log(req.body);
-//       const user_id_obj = await client
-//         .db("astronomy")
-//         .collection("globaluseridcounter")
-//         .find()
-//         .toArray();
-//       const user_id = user_id_obj[0].count
-//       console.log(user_id);
-//       user_profile_obj = {
-//         userid: user_id,
+router.post("/create", async (req, res) => {
+  try {
+    await client.connect();
+    console.log("Successfully connected to the database");
+    try {
+      console.log(req.body);
+      const user_id = uuid.v4();
+      console.log(user_id);
+      user_profile_response = {
+        userid: user_id,
+        message: "Successfully created user profile",
+      };
+      await client.db("astronomy").collection("users").insertOne({
+        userid: user_id,
+        email: req.body.email,
+        distance: req.body.distance,
+        maxnumberofobservatories: req.body.maxnumberofobservatories,
+      });
+      // logic for update the next user_id
+      await client
+        .db("astronomy")
+        .collection("globaluseridcounter")
+        .replaceOne({ count: user_id }, { count: user_id + 1 });
+      console.log("Successfully incremented user id....");
+      console.log("Returning this response to user....");
+      console.log(user_profile_response);
+      res.send(user_profile_response);
+    } catch (err) {
+      res.status(400).send(err);
+    }
+  } catch (err) {
+    console.log(err);
+    await client.close();
+  }
+});
 
-//       };
-//     } catch (err) {
-//       res.status(400).send(err);
-//     }
-//   } catch (err) {
-//     console.log(err);
-//     await client.close();
-//   }
-// });
-
-router.put(
+router.post(
   "/update/:userid/:username/:email/:distance/:maxnumberofobservatories",
   async (req, res) => {
     try {
@@ -73,11 +83,6 @@ router.put(
           distance,
           maxnumberofobservatories
         );
-        // req.body.userid = userid;
-        // req.body.username = username;
-        // req.body.email = email;
-        // req.body.distance = distance;
-        // req.body.maxnumberofobservatories = maxnumberofobservatories;
 
         await client.db("astronomy").collection("users").insertOne(req.body);
 
