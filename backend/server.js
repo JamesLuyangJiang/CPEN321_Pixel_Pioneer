033a8449f73250f7cbd3c98603ae7850ce42c78f
Jiayu Huang <harthuang990517@gmail.com>
2023-11-02 14:45:25 -0700

    refactor events.js to scheduling.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const uri = "mongodb://localhost:27017";
 const client = new MongoClient(uri);
 
 const recommendationsRoutes = require("./routes/recommendations");
-const schedulingRoutes = require("./routes/events");
+const schedulingRoutes = require("./routes/scheduling");
 const userRoutes = require("./routes/users");
 
 app.use(express.json());
