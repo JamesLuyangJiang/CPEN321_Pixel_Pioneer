54811b429de941b15beb8acbb58c851d1cf3abd8
Jiayu Huang <harthuang990517@gmail.com>
2023-11-03 11:01:56 -0700

    add invitation functionality

--- backend/routes/invite.js
+++ backend/routes/invite.js
@@ -1,4 +1,36 @@
 const express = require("express");
 const router = express.Router();
 
+app.post("/:userid", async (req, res) => {
+  const { userid } = req.params;
+  console.log(req.body.name);
+  console.log(req.body.receiver);
+  // TODO: Check if userid exists, if it exists, find the name and date of the event
+  const checkEventExists = await client
+    .db("astronomy")
+    .collection("events")
+    .findOne({ userid: userid, name: req.body.name });
+  if (!checkEventExists) {
+    res.status(400).send("EVENT NOT FOUND IN DATABASE.");
+  } else {
+    const eventName = checkEventExists.name;
+    const eventDate = checkEventExists.date;
+    // TODO: Check if receiver email exists in database
+    const checkReceiverEmailExists = await client
+      .db("astronomy")
+      .collection("users")
+      .findOne({ email: req.body.receiver });
+    if (!checkReceiverEmailExists) {
+      res.status(400).send("INVITED USER NOT FOUND IN DATABASE.");
+    } else {
+      await client.db("astronomy").collection("events").insertOne({
+        userid: checkReceiverEmailExists.userid,
+        name: eventName,
+        date: eventDate,
+      });
+      res.status(200).send("INVITE USER SUCCESSFUL.");
+    }
+  }
+});
+
 module.exports = router;
