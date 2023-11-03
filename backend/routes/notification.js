e04c20109a5b6a534ec62df41db7c7a3d9a081b1
whh1123 <58422140+whh1123@users.noreply.github.com>
2023-11-02 20:18:33 -0700

    Update notification.js

--- backend/routes/notification.js
+++ backend/routes/notification.js
@@ -1,22 +1,29 @@
+const express = require("express");
+const app = express();
 // Firebase Cloud Messaging
 var admin = require("firebase-admin");
-
 const moment = require('moment');
 
-// This function is finished with the help of ChatGPT, especially for the usage of "moment"
-function calculateTTLOnSeconds(dateToNotify) {
-//    // Parse the "dateToNotify" string into a Date object using moment.js
-//    dateToNotifyStr = dateToNotifyStr + " 00:00"
-//    const parsedDateToNotify = moment(dateToNotifyStr, 'YYYY-MM-DD HH:mm');
-    // Get the current date and time
-    const currentDateAndTime = moment();
-    // Calculate the time difference in seconds
-    const timeDifferenceInSeconds = dateToNotify.diff(currentDateAndTime, 'seconds');
-    return timeDifferenceInSeconds;
+//// This function is finished with the help of ChatGPT, especially for the usage of "moment"
+//function calculateTTLOnSeconds(dateToNotify) {
+////    // Parse the "dateToNotify" string into a Date object using moment.js
+////    dateToNotifyStr = dateToNotifyStr + " 00:00"
+////    const parsedDateToNotify = moment(dateToNotifyStr, 'YYYY-MM-DD HH:mm');
+//    // Get the current date and time
+//    const currentDateAndTime = moment();
+//    // Calculate the time difference in seconds
+//    const timeDifferenceInSeconds = dateToNotify.diff(currentDateAndTime, 'seconds');
+//    return timeDifferenceInSeconds;
+//}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
-async function sendNotification(registrationToken, dateToNotify) {
+
+async function sendNotification(registrationToken, dateToNotify, astronomy_data) {
     //  const time_to_live = Number(calculateTTLOnSeconds(dateToNotify));
-  const messageBody = "New event has been added for date " + dateToNotify;
+  await sleep(15000);
+  const messageBody = "Your scheduled star observation on " + dateToNotify + ": a sunrise at " + astronomy_data.sunrise + " and a sunset at " + astronomy_data.sunset;
   var message = {
     notification: {
       title: "New event registered!",
@@ -25,15 +32,12 @@ async function sendNotification(registrationToken, dateToNotify) {
     token: registrationToken
   };
 
-  await admin
-    .messaging()
-    .send(message)
-    .then(async function (response) {
-      console.log("Successfully sent with response: ", response);
-    })
-    .catch(async function (err) {
-      console.log("Something went wrong: " + err);
-    });
+  try {
+    const response = await admin.messaging().send(message);
+    console.log("Successfully sent with response: ", response);
+  } catch (err) {
+    console.log("Something went wrong: " + err);
+  }
 }
 
 module.exports = { sendNotification };
