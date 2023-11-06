5c0b3ccc7e736a97a7d4a40c75a627575319a968
Jiayu Huang <harthuang990517@gmail.com>
2023-11-06 11:11:15 -0800

    refactor invite.js based on Codacy

--- backend/routes/invite.js
+++ backend/routes/invite.js
@@ -9,15 +9,9 @@ const client = new MongoClient(uri);
 // ChatGPT usage: NO
 // Utility function to connect to our MongoDB database
 async function connectDB() {
-  try {
-    await client.connect();
-    console.log("Successfully connected to the database");
-    return true;
-  } catch (err) {
-    console.log(err);
-    await client.close();
-    return false;
-  }
+  await client.connect();
+  console.log("Successfully connected to the database");
+  return true;
 }
 
 // ChatGPT usage: NO
@@ -35,14 +29,19 @@ async function inviteSpecifiedUser(req, res) {
       const usersCollection = client.db("astronomy").collection("users");
 
       // Check if userid exists, if it exists, find the name and date of the event
-      const checkEventExists = await eventsCollection.findOne({ userid, name: req.body.name });
+      const checkEventExists = await eventsCollection.findOne({
+        userid,
+        name: req.body.name,
+      });
       if (!checkEventExists) {
         res.status(400).send("EVENT NOT FOUND IN DATABASE.");
       }
       const eventName = checkEventExists.name;
       const eventDate = checkEventExists.date;
       // Check if receiver email exists in database
-      const checkReceiverEmailExists = await usersCollection.findOne({ email: req.body.receiver });
+      const checkReceiverEmailExists = await usersCollection.findOne({
+        email: req.body.receiver,
+      });
       if (!checkReceiverEmailExists) {
         res.status(400).send("INVITED USER NOT FOUND IN DATABASE.");
       }
@@ -63,9 +62,13 @@ async function inviteSpecifiedUser(req, res) {
         getSenderProfile.email
       );
       return res.status(200).send("INVITE USER SUCCESSFUL.");
-
     } catch (err) {
       return res.status(400).send(err);
+    } finally {
+      if (client) {
+        await client.close();
+        console.log("Database connection closed");
+      }
     }
   }
 }
