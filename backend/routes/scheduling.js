5b2d7adcb78bf01f1584c7778c2cc25d396ef986
Jiayu Huang <harthuang990517@gmail.com>
2023-11-06 10:56:52 -0800

    refactor connectDB() function from Codacy feedback

--- backend/routes/scheduling.js
+++ backend/routes/scheduling.js
@@ -7,16 +7,9 @@ const client = new MongoClient(uri);
 
 // ChatGPT usage: NO
 async function connectDB() {
-  try {
-    await client.connect();
-    console.log("Successfully connected to the database");
-    return true;
-  } catch (err) {
-    console.log(err);
-    return false;
-  } finally {
-    await client.close();
-  }
+  await client.connect();
+  console.log("Successfully connected to the database");
+  return true;
 }
 
 // ChatGPT usage: Partial
