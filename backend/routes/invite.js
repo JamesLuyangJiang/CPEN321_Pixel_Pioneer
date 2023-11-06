000a1d97b92f28af30f5cc6a8e25a91a63d47312
whh1123 <58422140+whh1123@users.noreply.github.com>
2023-11-05 22:19:59 -0800

    Update invite.js refining codacy issues

--- backend/routes/invite.js
+++ backend/routes/invite.js
@@ -30,48 +30,42 @@ async function inviteSpecifiedUser(req, res) {
       const { userid } = req.params;
       console.log(req.body.name);
       console.log(req.body.receiver);
+
+      const eventsCollection = client.db("astronomy").collection("events");
+      const usersCollection = client.db("astronomy").collection("users");
+
       // Check if userid exists, if it exists, find the name and date of the event
-      const checkEventExists = await client
-        .db("astronomy")
-        .collection("events")
-        .findOne({ userid: userid, name: req.body.name });
+      const checkEventExists = await eventsCollection.findOne({ userid, name: req.body.name });
       if (!checkEventExists) {
         res.status(400).send("EVENT NOT FOUND IN DATABASE.");
-      } else {
-        const eventName = checkEventExists.name;
-        const eventDate = checkEventExists.date;
-        // Check if receiver email exists in database
-        const checkReceiverEmailExists = await client
-          .db("astronomy")
-          .collection("users")
-          .findOne({ email: req.body.receiver });
-        if (!checkReceiverEmailExists) {
-          res.status(400).send("INVITED USER NOT FOUND IN DATABASE.");
-        } else {
-          const getSenderProfile = await client
-            .db("astronomy")
-            .collection("users")
-            .findOne({ userid: userid });
-          console.log("PRINT INVITED USER INFO...");
-          console.log(checkReceiverEmailExists);
-          console.log("PRINT SENDER EMAIL...");
-          console.log(getSenderProfile.email);
-          await client.db("astronomy").collection("events").insertOne({
-            userid: checkReceiverEmailExists.userid,
-            name: eventName,
-            date: eventDate,
-          });
-          await notification.sendInviteNotification(
-            checkReceiverEmailExists.notificationToken,
-            eventDate,
-            eventName,
-            getSenderProfile.email
-          );
-          res.status(200).send("INVITE USER SUCCESSFUL.");
-        }
       }
+      const eventName = checkEventExists.name;
+      const eventDate = checkEventExists.date;
+      // Check if receiver email exists in database
+      const checkReceiverEmailExists = await usersCollection.findOne({ email: req.body.receiver });
+      if (!checkReceiverEmailExists) {
+        res.status(400).send("INVITED USER NOT FOUND IN DATABASE.");
+      }
+      const getSenderProfile = await usersCollection.findOne({ userid });
+      console.log("PRINT INVITED USER INFO...");
+      console.log(checkReceiverEmailExists);
+      console.log("PRINT SENDER EMAIL...");
+      console.log(getSenderProfile.email);
+      await eventsCollection.insertOne({
+        userid: checkReceiverEmailExists.userid,
+        name: eventName,
+        date: eventDate,
+      });
+      await notification.sendInviteNotification(
+        checkReceiverEmailExists.notificationToken,
+        eventDate,
+        eventName,
+        getSenderProfile.email
+      );
+      return res.status(200).send("INVITE USER SUCCESSFUL.");
+
     } catch (err) {
-      res.status(400).send(err);
+      return res.status(400).send(err);
     }
   }
 }
