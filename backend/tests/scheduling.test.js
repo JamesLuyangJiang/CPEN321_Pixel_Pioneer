a2a394022b782b500d0a024379a92a42ca6fbcbb
Jiayu Huang <harthuang990517@gmail.com>
2023-11-16 22:11:47 -0800

    add a test without mocking database

--- backend/tests/scheduling.test.js
+++ backend/tests/scheduling.test.js
@@ -1,5 +1,23 @@
-describe("scheduling", () => {
-  test("scheduling stub test #1", () => {
-    console.log("hello world");
+const {
+  getScheduledEvents,
+  createScheduledEvent,
+  deleteScheduledEvent,
+} = require("../routes/scheduling");
+
+// Interface POST https://pixelpioneer.canadacentral.cloudapp.azure.com:8081/scheduling/:userid/events
+describe("Create an event", () => {
+  test("Add an event unsuccessfully because ID does not exist", async () => {
+    const req = {
+      params: { id: 1 },
+      body: { name: "Kitslano Beach", date: "2023-11-06" },
+    };
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      send: jest.fn(),
+    };
+    await createScheduledEvent(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.send).toHaveBeenCalledWith("FAILED because ID does not exist in database.");
   });
 });
