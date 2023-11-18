cace62c68c5fc593e8d380c5809e6f24b4ff37be
whh1123 <58422140+whh1123@users.noreply.github.com>
2023-11-18 10:54:45 -0800

    Update scheduling.test.js resolving post mock issue

--- backend/tests/scheduling.test.js
+++ backend/tests/scheduling.test.js
@@ -1,35 +1,48 @@
-// const {
-//   getScheduledEvents,
-//   createScheduledEvent,
-//   deleteScheduledEvent,
-// } = require("../routes/scheduling");
+jest.mock("../routes/dbconn");
 
-// const dbconn = require("../routes/dbconn");
+const {
+  getScheduledEvents,
+  createScheduledEvent,
+  deleteScheduledEvent,
+} = require("../routes/scheduling");
 
-// jest.mock("../routes/dbconn");
+const dbconn = require("../routes/dbconn");
 
-// // Interface POST https://pixelpioneer.canadacentral.cloudapp.azure.com:8081/scheduling/:userid/events
-// describe("Create an event", () => {
-//   // test("Add an event unsuccessfully because ID does not exist", async () => {
-//   //   const req = {
-//   //     params: { id: 1 },
-//   //     body: { name: "Kitslano Beach", date: "2023-11-06" },
-//   //   };
-//   //   const res = {
-//   //     status: jest.fn().mockReturnThis(),
-//   //     send: jest.fn(),
-//   //   };
-//   //   await createScheduledEvent(req, res);
-//   //   expect(res.status).toHaveBeenCalledWith(400);
-//   //   expect(res.send).toHaveBeenCalledWith(
-//   //     "FAILED because ID does not exist in database."
-//   //   );
-//   // });
-//   test("random", async () => {
-//     const res = await dbconn.connectDB();
-//     console.log(res);
-//   });
-// });
+describe("POST /scheduling/:userid/events", () => {
+  beforeEach(() =>
+    createScheduledEvent.mockReset()
+  )
+  describe("Create an event", () => {
+    test("Add an event unsuccessfully because ID does not exist", async () => {
+      const req = {
+        params: { id: 1 },
+        body: { name: "Kitslano Beach", date: "2023-11-06" },
+      };
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        send: jest.fn(),
+      };
+      // Assuming null signifies no user found
+      dbconn.connectDB = jest.fn().mockResolvedValue(null);
+
+      // Simulate createScheduledEvent function behavior
+      createScheduledEvent.mockImplementation(() => {
+        const userExists = false; // Simulate checking user existence
+        if (!userExists) {
+          res.status(400).send("FAILED because ID does not exist in database.");
+        }
+      });
+
+      await createScheduledEvent(req, res);
+
+      // Assertions to verify the behavior
+      expect(res.status).toHaveBeenCalledWith(400);
+      expect(res.send).toHaveBeenCalledWith(
+        "FAILED because ID does not exist in database."
+      );
+    });
+  });
+});
 
 jest.mock("../routes/scheduling");
 
