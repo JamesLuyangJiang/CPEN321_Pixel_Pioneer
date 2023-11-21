96ba93eea1bb56707918418793977f13f62579d6
whh1123 <58422140+whh1123@users.noreply.github.com>
2023-11-20 23:42:59 -0800

    Update recommendations.js

--- backend/routes/recommendations.js
+++ backend/routes/recommendations.js
@@ -13,7 +13,6 @@ async function recommendationRequestHandler(req, res) {
   const userid = req.params.userid;
   const days = req.params.days;
   const distance = await getUserDistance(userid);
-
   const maximum_city_number = 10;
   const publicIP = req.ip.substring(7);
 
@@ -50,13 +49,10 @@ async function getUserDistance(userid) {
         .db("astronomy")
         .collection("users")
         .findOne({ userid });
-
-      if (!user) {
-        return 1000; // Default distance
-      }
-      return Number(user.distance) || 1000; // Set distance from user's data. If its null, set 1000
+      // Set distance from user's data. If its null, set 1000
+      return user && user.distance ? Number(user.distance) : 1000;
     } catch (err) {
-      console.log(err);
+      console.error("Error fetching user distance:", err);
     } finally {
     await client.close();
   }
@@ -70,48 +66,71 @@ function generateRecommendationList(
   distance_preference,
   maximum_city_number
 ) {
+  if (!Array.isArray(nearby_observatory_list) || !nearby_observatory_list.length) {
+    throw new Error('Invalid observatory list');
+  }
+
+  // Remove duplicates from the list based on the name
+  const uniqueObservatoryList = nearby_observatory_list
+    .reduce((acc, current) => {
+      const x = acc.find(item => item.name === current.name);
+      if (!x) {
+        return acc.concat([current]);
+      } else {
+        return acc;
+      }
+    }, []);
+
   // Calculate minDistance and maxDistance using reduce
-  const { min_distance, max_distance } = nearby_observatory_list
+  const { min_distance, max_distance } = uniqueObservatoryList
     .filter((item) =>
-      item.weatherForecast.some((forecast) => forecast.condition_score > 3)
+      item.weatherForecast && item.weatherForecast.some((forecast) => forecast.condition_score > 3)
     )
     .reduce(
       (acc, item) => {
         const item_distance = Number(item.distance);
-        if (item_distance < acc.min_distance) {
-          acc.min_distance = item_distance;
-        }
-        if (item_distance > acc.max_distance) {
-          acc.max_distance = item_distance;
-        }
+        acc.min_distance = Math.min(acc.min_distance, item_distance);
+        acc.max_distance = Math.max(acc.max_distance, item_distance);
         return acc;
       },
-      { min_distance: 0.01, max_distance: distance_preference }
+      { min_distance: Infinity, max_distance: 0 }
     );
 
-  // Calculate total scores and sort the recommendation list in one pass
-  const recommendation_list = nearby_observatory_list
-    .map((item) => {
+  // Calculate total scores and sort the recommendation list
+  const recommendation_list = uniqueObservatoryList
+    .map(item => {
+      // Choose the forecast with the highest date score
+      const bestForecast = item.weatherForecast.reduce((best, current) => {
+        return (best.condition_score * best.date_score > current.condition_score * current.date_score) ? best : current;
+      }, { condition_score: -1 });
+
+      if (!bestForecast || bestForecast.condition_score === -1) {
+        return null;
+      }
+
       const item_distance = Number(item.distance);
       const weighted_distance_score =
-        (1 - (item_distance - min_distance) / (max_distance - min_distance)) *
-        0.4;
-      const weighted_condition_score =
-        (item.weatherForecast[0].condition_score * 0.6) / 9;
+        (1 - (item_distance - min_distance) / (max_distance - min_distance)) * 0.3;
+      const weighted_condition_score = (bestForecast.condition_score * 0.5) / 9;
+      const weighted_date_score = (bestForecast.date_score * 0.2);
+
       const total_score =
-        (weighted_distance_score + weighted_condition_score) * 100;
+        (weighted_distance_score + weighted_condition_score + weighted_date_score) * 100;
+
       return {
         name: item.name,
         distance: item_distance.toFixed(2),
-        date: item.weatherForecast[0].date,
-        condition: item.weatherForecast[0].condition,
+        date: bestForecast.date,
+        condition: bestForecast.condition,
         total_score: total_score.toFixed(2),
       };
     })
+    .filter(item => item !== null)
     .sort((a, b) => b.total_score - a.total_score);
 
-  return recommendation_list.slice(0, maximum_city_number + 1);
+  return recommendation_list.slice(0, maximum_city_number);
 }
+
 
 router.get("/:userid/:days", recommendationRequestHandler);
 
