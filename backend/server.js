4e9d23f99e68185189d6b7ee1eb9730cefd427a2
Jiayu Huang <harthuang990517@gmail.com>
2023-11-26 21:37:25 -0800

    start https server

--- backend/server.js
+++ backend/server.js
@@ -9,19 +9,19 @@ const port = 8081;
 const server = http.createServer(app);
 
 // FOR LOCAL TESTING, USE THIS HTTP SERVER
-server.listen(port, async () => {
-  console.log(`HTTP server listening on port ${port}`);
-});
+// server.listen(port, async () => {
+//   console.log(`HTTP server listening on port ${port}`);
+// });
 
 // FOR VM TESTING, USE THIS HTTPS SERVER
-// https
-//   .createServer(
-//     {
-//       key: fs.readFileSync("privkey.pem"),
-//       cert: fs.readFileSync("cert.pem"),
-//     },
-//     app
-//   )
-//   .listen(port, async () => {
-//     console.log(`HTTPS server listening on port ${port}`);
-//   });
+https
+  .createServer(
+    {
+      key: fs.readFileSync("privkey.pem"),
+      cert: fs.readFileSync("cert.pem"),
+    },
+    app
+  )
+  .listen(port, async () => {
+    console.log(`HTTPS server listening on port ${port}`);
+  });
