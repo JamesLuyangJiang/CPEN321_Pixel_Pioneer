7f4400e0598a0cedb333e0f11c36742f256fbb7e
Jiayu Huang <harthuang990517@gmail.com>
2023-11-27 22:17:28 -0800

    use https and comment out http server

--- backend/server.js
+++ backend/server.js
@@ -9,23 +9,23 @@ const port = 8081;
 const server = http.createServer(app);
 
 // FOR LOCAL TESTING, USE THIS HTTP SERVER
-server.listen(port, async () => {
-  console.log(`HTTP server listening on port ${port}`);
-});
+// server.listen(port, async () => {
+//   console.log(`HTTP server listening on port ${port}`);
+// });
 
 // FOR VM TESTING, USE THIS HTTPS SERVER
-// https
-//   .createServer(
-//     {
-//       key: fs.readFileSync(
-//         "/home/azureuser/CPEN321_Pixel_Pioneer/backend/privkey.pem"
-//       ),
-//       cert: fs.readFileSync(
-//         "/home/azureuser/CPEN321_Pixel_Pioneer/backend/cert.pem"
-//       ),
-//     },
-//     app
-//   )
-//   .listen(port, async () => {
-//     console.log(`HTTPS server listening on port ${port}`);
-//   });
+https
+  .createServer(
+    {
+      key: fs.readFileSync(
+        "/home/azureuser/CPEN321_Pixel_Pioneer/backend/privkey.pem"
+      ),
+      cert: fs.readFileSync(
+        "/home/azureuser/CPEN321_Pixel_Pioneer/backend/cert.pem"
+      ),
+    },
+    app
+  )
+  .listen(port, async () => {
+    console.log(`HTTPS server listening on port ${port}`);
+  });
